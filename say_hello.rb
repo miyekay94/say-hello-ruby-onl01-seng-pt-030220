44ec06b22670d2d1333bc32c626c038c81e3baf4
Mary Kanu <maryikanu@gmail.com>
2020-03-09 10:11:00 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,9 @@
 # Build your say_hello method here
 
-##Assigning A Default Value
-
-def say_hello(name = "Ruby programer")
-  puts "Hello, #{{name}}!"
+def say_hello(name = "ruby programer")
+  puts "Hello, #{name}"
 end
 
-##Calling the method
-say_hello("Mary")
-
 say_hello
+
+say_hello ("Mary")
